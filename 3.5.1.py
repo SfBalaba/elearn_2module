f16a879bb666b0bb275a0a487668b110ac987390
BalabaSofia <102922461+SfBalaba@users.noreply.github.com>
2022-12-28 22:09:29 +0500

    add currencies_db

--- 3.5.1.py
+++ 3.5.1.py
@@ -10,9 +10,11 @@ def createSqlFromCSV(file_name: str) -> None:
     df = pd.read_csv(file_name)
     conn = sqlite3.connect('currencies_db')
     cursor = conn.cursor()
+    # cursor.execute("""drop table currencies""")
     cursor.execute('CREATE TABLE currencies (date text, RUR number, USD number, KZT number, BYR number,'
               'UAH number, EUR number)')
     conn.commit()
-    df.to_sql('currencies', conn, index=False)
+    df.to_sql('currencies', conn, if_exists='replace', index=False)
+    conn.close()
 
 createSqlFromCSV('currencies.csv')
