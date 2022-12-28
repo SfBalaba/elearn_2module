cea8dedd53d2d416557f8b9857f0912287987c62
BalabaSofia <102922461+SfBalaba@users.noreply.github.com>
2022-12-28 21:52:02 +0500

    currencies to db

---
+++ 3.5.1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import sqlite3
+
+def createSqlFromCSV(file_name: str) -> None:
+    """
+    Создаёт таблицу currencies базы данных currencies.db из CSV-файла
+    :param file_name: название файла csv из  корня проекта
+    :return: None
+    """
+    df = pd.read_csv(file_name)
+    conn = sqlite3.connect('currencies_db')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE currencies (date text, RUR number, USD number, KZT number, BYR number,'
+              'UAH number, EUR number)')
+    conn.commit()
+    df.to_sql('currencies', conn, index=False)
+
+createSqlFromCSV('currencies.csv')
